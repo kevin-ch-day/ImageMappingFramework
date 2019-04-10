6d209299bec036220700624bb04a762a2d8a695d
kevin-ch-day <kevin.ch.day@gmail.com>
2019-04-10 17:31:55 -0500

    added to projects 410219
    Added the admin and preferences pages to web application

---
+++ admin.php
@@ -0,0 +1,15 @@
+<?php
+require_once('includes\header.inc');
+/*
+ * #1 - Admin can specify an image and create links (rectangular areas)
+ * #2 - Admin can delete, update the previously created links
+ * #3 - Admin can submit / persist the data into the database
+ * #4 - Admin is able to retrieve the existing links
+ */
+?>
+
+<h1>Admin</h1>
+
+<?php
+require_once('includes\footer.inc');
+?>
---
+++ preferences.php
@@ -0,0 +1,9 @@
+<?php
+require_once('includes\header.inc');
+?>
+
+<h1>Preferences</h1>
+
+<?php
+require_once('includes\footer.inc');
+?>
