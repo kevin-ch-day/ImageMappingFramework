eed7a645af48cc314a1a79d4c75491e481a53a2f
kevin day <oh3080hr@metrostate.edu>
2019-04-10 14:51:36 -0500

    Project Iteration #3

--- register.php
+++ register.php
@@ -48,8 +48,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate password
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter a password.";     
+
     } elseif(strlen(trim($_POST["password"])) < 6){
         $password_err = "Password must have atleast 6 characters.";
+        
     } else{
         $password = trim($_POST["password"]);
     }
