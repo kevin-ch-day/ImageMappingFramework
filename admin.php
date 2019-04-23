a1d1f59d9d6eaf974dcf5571dd22529e36f01d42
kevin day <oh3080hr@metrostate.edu>
2019-04-22 22:35:26 -0500

    creating the admin area
    Creating the admin area where links and titles can be edited

--- admin.php
+++ admin.php
@@ -6,10 +6,37 @@ require_once('includes\header.inc');
  * #3 - Admin can submit / persist the data into the database
  * #4 - Admin is able to retrieve the existing links
  */
-?>
 
-<h1>Admin</h1>
+echo "<h1>Admin</h1>";
 
-<?php
+mysqli_select_db($conn, "imageframework");
+
+$query = "SELECT * FROM imagelinks";
+$result = aQuery($query);
+if($result->num_rows > 0 ){
+    echo "<br/><table><thead><tr>";
+        echo "<th>URL</th>";
+        echo "<th>Name</th>";
+        echo "<th>Target</th>";
+        echo "<th>Coordinates</th>";
+        echo "<th>Edit</th></tr></thead><tbody>";
+
+        while($row = $result->fetch_array()){
+            echo "<tr>";
+            echo "<td><a href=\"" . $row['link_url'] ."\" target=\"_blank\">Link</a></td>";
+            echo "<td>" . $row['link_name'] . "</td>";
+            echo "<td>" . $row['link_target'] . "</td>";
+            echo "<td>" . $row['link_coords'] . "</td>";
+            echo "<td><td></tr>";
+        }
+
+        echo "</tbody></table>";
+        // Free result set
+        $result->free();
+
+} else{
+    echo "<p><em>No records were found.</em></p>";
+}
+
 require_once('includes\footer.inc');
 ?>
--- generateImageMap.php
+++ generateImageMap.php
@@ -7,18 +7,16 @@ $name = $_POST["name"];
 $target = $_POST["target"];
 $coords = $_POST["coords"];
 
-echo "<b>Link: $url<b><br/>";
-echo "<b>Title: $name<b><br/>";
-echo "<b>Target: $target<b><br/>";
-echo "<b>Coords: $coords<b><br/>";
+echo "<b>Link: $url</b><br/>";
+echo "<b>Title: $name</b><br/>";
+echo "<b>Target: $target</b><br/>";
+echo "<b>Coords: $coords</b><br/>";
 
 
 mysqli_select_db($conn, "imageframework");
 
 $query = "INSERT INTO imagelinks (link_url, link_name, link_target, link_coords)
 VALUES ('$url', '$name', '$target', '$coords')";
-
-aQuery($query);
 
 require_once('includes\footer.inc');
 ?>
