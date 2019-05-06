4988ef1dcf8c25a86fdf184d37d5daf7142bdf78
kevin day <oh3080hr@metrostate.edu>
2019-05-06 00:27:25 -0500

    a

--- index.php
+++ index.php
@@ -15,7 +15,20 @@ if($result->num_rows > 0 ){
 echo "<div id=\"content\"><div id=\"main-menu\">";
 
 if(isset($image)){
-    echo "<img src=\"images\\$image\" alt=\"Image\">";
+    echo "<img src=\"images\\$image\" alt=\"Image\" usemap=\"#imageMap\">";
+
+    echo "<map name=\"imageMap\">";
+    mysqli_select_db($conn, "imageframework");
+    $query = "SELECT * FROM imagelinks";
+    $result = aQuery($query);
+    if($result->num_rows > 0 ){
+        while($row = $result->fetch_array()){
+            echo "<area shape=\"rect\" coords=\"".$row['link_coords']."\" href=\"".$row['link_url']."\" target=\"".$row['link_target']."\">";
+        }
+        $result->free();
+    }
+    echo "</map>";
+        
 
 }else{
     echo "<h1>No image uploaded yet</h1>";
